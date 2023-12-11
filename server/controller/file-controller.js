80ef4135879c1046e087fc55e64ad7ee36925ea5
arbaz <arbazch1313@gmail.com>
2023-12-11 15:53:43 +0500

    send docx file

--- server/controller/file-controller.js
+++ server/controller/file-controller.js
@@ -1,6 +1,8 @@
 import ChatGPTService from "../services/gpt-service.js";
 import TextService from "../utils/extract-text.js";
 import MongoService from "../services/mongo-service.js";
+import DocumentCreator from "../utils/document-maker.js";
+import { Packer } from "docx";
 
 
 export default class FileController {
@@ -21,11 +23,14 @@ export default class FileController {
   static getDocumentFiles = async (req, res) => {
     const { id } = req.params;
     try {
-      const data = await MongoService.GetData(id);
-
-      //?? Make a document (docx) from this json data
-
-      res.status(200).send(data) 
+      //const data = await MongoService.GetData(id);
+      const document = DocumentCreator.create();
+      const b64string = await Packer.toBase64String(document);
+      const buffer = Buffer.from(b64string, 'base64');
+      res.setHeader('Content-Disposition', 'attachment; filename=MyDocument.docx');
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+      res.status(200).send(buffer);
+      
     } catch (error) {
       return res.status(500).json({
         message: error,
